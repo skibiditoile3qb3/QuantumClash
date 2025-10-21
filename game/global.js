a6618c7633b47cca6a59aaadaf6cba2d73622a8f
skibiditoile3qb3 <jijojak@usd437.net>
2025-10-21 09:33:03 -0500

    Add warning function for console messages

---
+++ game/global.js
@@ -0,0 +1,6 @@
+(function() {
+  window.warning = (msg) => {
+    console.log(`%c⚠️ ${msg}`, 'color: #ff3333; font-weight: bold; background: #1a0000; padding: 4px 8px; border-radius: 4px;');
+  };
+
+})();
