5feeccc3f119617abec780dc952ba35b4d30e969
skibiditoile3qb3 <jijojak@usd437.net>
2025-10-29 16:47:08 -0500

    Update global.js

--- game/global.js
+++ game/global.js
@@ -1,6 +1,3 @@
-(function() {
-  window.warning = (msg) => {
-    console.log(`%c⚠️ ${msg}`, 'color: #ff3333; font-weight: bold; background: #1a0000; padding: 4px 8px; border-radius: 4px;');
-  };
-
-})();
+let isAIGame = false;
+let ai = null;
+let isAITurn = false;
